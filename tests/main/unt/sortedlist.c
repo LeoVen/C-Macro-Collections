63d04533413d1fd44abf41ed308dd6dd082b0f41
Leonardo <lvenk26@gmail.com>
2019-10-11 02:14:33 -0300

    Finally releasing sortedlist

--- tests/main/unt/sortedlist.c
+++ tests/main/unt/sortedlist.c
@@ -51,7 +51,7 @@ CMC_CREATE_UNIT(sortedlist_test, true, {
     });
 
     CMC_CREATE_TEST(buffer_growth[capacity = 1], {
-        sortedlist *sl = sl_new(1, cmp);
+        sortedlist *sl = sl_new(100, cmp);
 
         cmc_assert_not_equals(ptr, NULL, sl);
 
@@ -62,6 +62,78 @@ CMC_CREATE_UNIT(sortedlist_test, true, {
 
         cmc_assert_equals(size_t, 50, sl_count(sl));
         cmc_assert_array_sorted_any(size_t, sl->buffer, cmp, 0, sl->count - 1);
+
+        sl_free(sl, NULL);
+    });
+
+    CMC_CREATE_TEST(insert, {
+        sortedlist *sl = sl_new(100, cmp);
+
+        cmc_assert_not_equals(ptr, NULL, sl);
+
+        cmc_assert(sl_insert(sl, 1));
+
+        sl_free(sl, NULL);
+    });
+
+    CMC_CREATE_TEST(remove, {
+        sortedlist *sl = sl_new(100, cmp);
+
+        cmc_assert_not_equals(ptr, NULL, sl);
+
+        cmc_assert(sl_insert(sl, 32));
+
+        cmc_assert(sl_remove(sl, 0));
+
+        sl_free(sl, NULL);
+    });
+
+    CMC_CREATE_TEST(remove[count = 0], {
+        sortedlist *sl = sl_new(100, cmp);
+
+        cmc_assert_not_equals(ptr, NULL, sl);
+
+        cmc_assert(!sl_remove(sl, 0));
+
+        sl_free(sl, NULL);
+    });
+
+    CMC_CREATE_TEST(remove[out of range], {
+        sortedlist *sl = sl_new(100, cmp);
+
+        cmc_assert_not_equals(ptr, NULL, sl);
+
+        cmc_assert(sl_insert(sl, 32));
+
+        cmc_assert(!sl_remove(sl, 2));
+
+        sl_free(sl, NULL);
+    });
+
+    CMC_CREATE_TEST(indexof, {
+        sortedlist *sl = sl_new(1, cmp);
+
+        cmc_assert_not_equals(ptr, NULL, sl);
+
+        for (size_t i = 0; i < 20; i++)
+            cmc_assert(sl_insert(sl, i % 10));
+
+        /* 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 */
+        cmc_assert_equals(size_t, 4, sl_indexof(sl, 2, true));
+        cmc_assert_equals(size_t, 5, sl_indexof(sl, 2, false));
+
+        cmc_assert_equals(size_t, 0, sl_indexof(sl, 0, true));
+        cmc_assert_equals(size_t, 1, sl_indexof(sl, 0, false));
+
+        cmc_assert(sl_remove(sl, 0));
+
+        cmc_assert_equals(size_t, 0, sl_indexof(sl, 0, true));
+        cmc_assert_equals(size_t, 0, sl_indexof(sl, 0, false));
+
+        cmc_assert(sl_remove(sl, 0));
+
+        cmc_assert_equals(size_t, sl_count(sl), sl_indexof(sl, 0, true));
+        cmc_assert_equals(size_t, sl_count(sl), sl_indexof(sl, 0, false));
 
         sl_free(sl, NULL);
     });
