78fe5c2b097ac36cd2ac5c67e016a012bdef3d60
Leonardo Vencovsky <lvenk26@gmail.com>
2023-02-28 16:42:49 +0100

    Libray

--- src/cmc/cmc/treemap/ext/code.h
+++ src/cmc/cmc/treemap/ext/code.h
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2019 Leonardo Vencovsky
  *
- * This file is part of the C Macro Collections Libray.
+ * This file is part of the C Macro Collections Library.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
